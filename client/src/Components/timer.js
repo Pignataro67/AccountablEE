8e6cf8554e893f132b4d831b09ad25a22180122c
Pignataro67 <Pignataro67@gmail.com>
2020-10-17 05:28:37 -0700

    stylin'

---
+++ client/src/Components/timer.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import ReactCountdownClock from "react-countdown-clock";
+
+  class Timer extends Component {
+
+    // constructor(){
+    //   super()
+    //   this.state = {
+    //     allSessions: []
+    //   }
+    // }
+
+    myCallback = () => {
+      return "Done";
+    };
+
+
+
+    render() {
+      const minutes = 20;
+      let pausation = false;
+      const toggle = (pausation) => {
+        console.log("clicked")
+       if (pausation === false) {
+           pausation = true;
+        } else {
+          pausation = false;}
+      };
+
+      return (
+        <ReactCountdownClock
+          seconds={60 * minutes}
+          color="#232323"
+          alpha={0.5}
+          size={100}
+          onComplete={this.myCallback}
+          paused={pausation}
+          weight={20}
+          onClick={()=> toggle(pausation)}
+        />
+      );
+    }
+  }
+
+export default Timer;
